7a7f93ed82551159ba8eaf1013c99b5b9c01c615
Mattia Malonni <mattia.malonni@spindox.it>
2021-11-03 15:20:52 +0100

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
 # sequelize-bcrypt
+
+## Installation
+
+```bash
+npm i sequelize-bcrypt
+```
+
+## Setup
+
+```javascript
+const { Sequelize, DataTypes } = require('sequelize');
+const SequelizeBcrypt = require('sequelize-bcrypt');
+
+const database = new Sequelize({
+  ...sequelizeConnectionOptions,
+});
+
+const User = database.define('User', {
+  email: { type: DataTypes.STRING },
+  password: { type: DataTypes.STRING },
+});
+
+SequelizeBcrypt.useBcrypt(User, options);
+```
+
+## Options
+
+```javascript
+{
+  field: 'password', // secret field to hash, default: 'password'
+  rounds: 12, // used to generate bcrypt salt, default: 12
+  compare: 'authenticate', // method used to compare secrets, default: 'authenticate'
+}
+```
+
+## Usage
+
+```javascript
+User.create({ email: 'john.doe@example.com', password: 'SuperSecret!' });
+// { id: 1, email: 'john.doe@example.com', password: '$2a$12$VtyL7j5xx6t/GmmAqy53ZuKJ1nwPox5kHLXDaottN9tIQBsEB3EsW' }
+
+const user = await User.findOne({ where: { email: 'john.doe@example.com' } });
+user.authenticate('WrongPassword!'); // false
+user.authenticate('SuperSecret!'); // true
+```
---
+++ index.js
@@ -0,0 +1,42 @@
+const { ValidationError } = require('sequelize');
+const bcrypt = require('bcryptjs');
+
+const DEFAULT_OPTIONS = {
+  field: 'password',
+  rounds: 12,
+  compare: 'authenticate',
+};
+
+class SequelizeBcrypt {
+  static useBcrypt(Model, options = DEFAULT_OPTIONS) {
+    const opts = { ...DEFAULT_OPTIONS, ...options };
+
+    const hashField = function (instance) {
+      try {
+        const changedKey = Array.from(instance._changed).find(key => key === opts.field);
+        if (!changedKey) return;
+
+        const fieldDefinition = instance.rawAttributes[changedKey];
+        if (!fieldDefinition) return;
+
+        const plainValue = instance[changedKey];
+        if (!plainValue) return;
+
+        const salt = bcrypt.genSaltSync(opts.rounds);
+        const hashedValue = bcrypt.hashSync(plainValue, salt);
+        instance[changedKey] = hashedValue;
+      } catch (err) {
+        throw new ValidationError(err.message);
+      }
+    };
+
+    Model.prototype[opts.compare] = function (plainValue) {
+      return bcrypt.compareSync(plainValue, this._previousDataValues[opts.field]);
+    };
+
+    Model.addHook('beforeCreate', hashField);
+    Model.addHook('beforeUpdate', hashField);
+  }
+}
+
+module.exports = SequelizeBcrypt;
